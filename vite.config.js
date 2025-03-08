6d13d5df377d8a6f2a6b282cd4aad8b04bfc2ca6
Justin Etienne <juetienne7@gmail.com>
2025-03-08 15:36:04 +0900

    redirects and netlify deleted, test link change

--- vite.config.js
+++ vite.config.js
@@ -5,22 +5,18 @@ export default {
     root: 'src/',
     publicDir: '../static/',
     base: './',
-    server:
-    {
+    server: {
         host: true, // Open to local network and display URL
         open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
     },
-    build:
-    {
+    build: {
         outDir: '../dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
+        sourcemap: true, // Add sourcemap
+        assetsInclude: ['projects/**/*'] // Include the projects folder and its contents
     },
-    plugins:
-    [
-        restart({ restart: [ '../static/**', ] }), // Restart server on static file change
+    plugins: [
+        restart({ restart: ['../static/**'] }), // Restart server on static file change
         glsl() // Handle shader files
     ]
 }
-
-
