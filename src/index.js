f2462c8255a3bb1d12f5af35df44f95c64336093
Justin Etienne <juetienne7@gmail.com>
2025-03-09 14:49:29 +0900

    redirects file added test

--- src/index.js
+++
@@ -1,2 +0,0 @@
-// import slimeAnimated from "/projects/slimeAnimated/index.html";
-// import slimeBaked from "/projects/slimeBaked/index.html";
--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,9 @@
-// import restart from 'vite-plugin-restart'
-// import glsl from 'vite-plugin-glsl'
-
-// export default {
-//     root: 'src/',
-//     publicDir: '../static/',
-//     base: './',
-//     server: {
-//         host: true, // Open to local network and display URL
-//         open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
-//     },
-//     build: {
-//         outDir: '../dist', // Output in the dist/ folder
-//         emptyOutDir: true, // Empty the folder first
-//         sourcemap: true, // Add sourcemap
-//         assetsInclude: ['projects/**/*', '**/*.html'] // Include the projects folder and its contents
-//     },
-//     plugins: [
-//         restart({ restart: ['../static/**'] }), // Restart server on static file change
-//         glsl() // Handle shader files
-//     ]
-// }
-
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
 import handlebars from 'vite-plugin-handlebars';
 import { pageData } from './src/consts/';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   root: resolve(__dirname, 'src'),
   base: '/threejs-sandbox/',
